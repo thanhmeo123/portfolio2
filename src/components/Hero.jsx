c620a643a15c12839cf4ac8c865e5667838d2026
Nguyễn Thành <thanhngune123@gmail.com>
2025-02-03 23:28:05 +0700

    fix content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                 variants={container(0)}
                 initial="hidden"
                 animate="visible"
-                className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+                className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-5xl"
               >
                 {t("myname")}
               </motion.h1>
